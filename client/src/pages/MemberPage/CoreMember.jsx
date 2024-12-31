78a0fb8025eb4f87bec2bc00ad3d8a50f87a3f37
Rishabh <rishabtripathi.9984@gmail.com>
2024-12-31 11:47:08 +0530

    Your descriptive commit message

--- client/src/pages/MemberPage/CoreMember.jsx
+++ client/src/pages/MemberPage/CoreMember.jsx
@@ -1,12 +1,13 @@
 const CoreMember = () => {
   return (
-    <div className="h-[750px]">
+    <div className="h-auto pt-8">
       <h1 className="text-white pt-12 pb-16 text-3xl text-center">
         Core Members
       </h1>
-      <div className="flex flex-col space-y-28 justify-center">
-        <div className="flex space-x-24 text-white justify-center">
-          <h1 className="w-[50%]">
+      <div className="flex flex-col space-y-16 justify-center">
+        {/* First Member Section */}
+        <div className="flex flex-col-reverse md:flex-row space-y-8 md:space-y-0 md:space-x-24 text-white items-center md:justify-center">
+          <h1 className="w-full md:w-[50%] text-center md:text-left">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore et
             beatae commodi impedit nulla qui mollitia, nobis eaque suscipit
             natus accusamus expedita placeat consequuntur nihil error sequi,
@@ -16,18 +17,29 @@ const CoreMember = () => {
             corrupti aperiam quis doloremque amet! Necessitatibus, harum.
           </h1>
           <div className="flex flex-col space-y-4 items-center justify-center">
-          <div
+            <div
               className="w-36 h-36 rounded-full bg-core-mem"
               style={{
                 backgroundColor: "rgba(56, 53, 127, 1)",
                 boxShadow: "0 0 40px 40px rgba(56, 53, 127, 0.5)",
                 backdropFilter: "blur(8px)",
               }}
-            ></div>            <h1>Lorem</h1>
+            ></div>
+            <h1>Lorem</h1>
           </div>
         </div>
 
-        <div className="flex space-x-24 text-white justify-center">
+        {/* Second Member Section */}
+        <div className="flex flex-col-reverse md:flex-row-reverse space-y-8 md:space-y-0 md:space-x-24 text-white items-center md:justify-center">
+          <h1 className="w-full md:w-[50%] text-center md:text-left">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore et
+            beatae commodi impedit nulla qui mollitia, nobis eaque suscipit
+            natus accusamus expedita placeat consequuntur nihil error sequi,
+            aliquam, tempore maxime! Lorem ipsum dolor sit amet consectetur,
+            adipisicing elit. Perferendis, rerum explicabo itaque fugit ab
+            maiores optio doloribus laborum et repellendus magni porro facere
+            corrupti aperiam quis doloremque amet! Necessitatibus, harum.
+          </h1>
           <div className="flex flex-col space-y-4 items-center justify-center">
             <div
               className="w-36 h-36 rounded-full bg-core-mem"
@@ -37,25 +49,15 @@ const CoreMember = () => {
                 backdropFilter: "blur(8px)",
               }}
             ></div>
-
             <h1>Lorem</h1>
           </div>
-          <h1 className="w-[50%]">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore et
-            beatae commodi impedit nulla qui mollitia, nobis eaque suscipit
-            natus accusamus expedita placeat consequuntur nihil error sequi,
-            aliquam, tempore maxime! Lorem ipsum dolor sit amet consectetur,
-            adipisicing elit. Perferendis, rerum explicabo itaque fugit ab
-            maiores optio doloribus laborum et repellendus magni porro facere
-            corrupti aperiam quis doloremque amet! Necessitatibus, harum.
-          </h1>
         </div>
       </div>
       <div className="w-full px-8 my-16">
         <hr className="bg-gray-400 w-full h-[0.1px]" />
       </div>
 
-      <div className="bg-grad-member relative left-[1200px] top-[-600px] "></div>
+      <div className="bg-grad-member relative left-[50%] md:left-[1200px] top-[-600px]"></div>
     </div>
   );
 };
--- client/src/pages/MemberPage/PodcastPage.jsx
+++ client/src/pages/MemberPage/PodcastPage.jsx
@@ -1,37 +1,36 @@
 import video from "../../Contents/PodCastPageContent";
+
 const PodcastPage = () => {
-    
-  
-    return (
-      <div className="h-[650px]">
-        <h1 className="text-white text-3xl text-center py-16 tracking-wide font-sans">
-          Podcast
-        </h1>
-        <div className="space-y-10">
-          {video.map((element, index) => (
-            <div key={index} className="text-white">
-              <div className="flex items-center space-x-24 justify-center">
-                <iframe
-                  className="rounded-3xl border border-gray-800"
-                  width="200"
-                  height="200"
-                  src={element.videourl}
-                  title={`YouTube video player ${index}`}
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  referrerPolicy="strict-origin-when-cross-origin"
-                  allowFullScreen
-                ></iframe>
-                <p className="w-[50%]">{element.content}</p>
-              </div>
+  return (
+    <div className="h-auto px-4 sm:px-8">
+      <h1 className="text-white text-3xl text-center py-8 tracking-wide font-sans">
+        Podcast
+      </h1>
+      <div className="space-y-10">
+        {video.map((element, index) => (
+          <div key={index} className="text-white">
+            <div className="flex flex-col md:flex-row items-center space-y-8 md:space-y-0 md:space-x-10 justify-center">
+              <iframe
+                className="rounded-3xl border border-gray-800"
+                width="200"
+                height="200"
+                src={element.videourl}
+                title={`YouTube video player ${index}`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              ></iframe>
+              <p className="w-full md:w-[50%] text-center md:text-left px-4 md:px-0">
+                {element.content}
+              </p>
             </div>
-          ))}
-        </div>
-  
-        <div className="bg-grad-member relative right-80 bottom-80"></div>
+          </div>
+        ))}
       </div>
-    );
-  };
-  
-  export default PodcastPage;
-  
+      <div className="bg-grad-member relative md:right-80 md:bottom-80 mt-10"></div>
+    </div>
+  );
+};
+
+export default PodcastPage;
